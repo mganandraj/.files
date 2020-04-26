a765e9260cf71752062ae169cb526c000dc6852f
Anandraj Govindan <anandrag@microsoft.com>
2020-04-26 13:03:12 -0700

    Fixing mono install script

--- ubuntu/mono.18.04.sh
+++ ubuntu/mono.18.04.sh
@@ -7,10 +7,18 @@ LSB_CODENAME=18.04
 # Test to see if the software in question is already installed, if not install it
 # wget "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF" -O out && sudo apt-key add out && rm out
 
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+# echo "deb https://download.mono-project.com/repo/ubuntu stable-$LSB_CODENAME main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+# apt-get update
+# apt-get install -y --no-install-recommends apt-transport-https mono-complete
+
+# From https://www.mono-project.com/download/stable/#download-lin
+apt install gnupg ca-certificates
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-$LSB_CODENAME main" | tee /etc/apt/sources.list.d/mono-official-stable.list
-apt-get update
-apt-get install -y --no-install-recommends apt-transport-https mono-complete
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+apt update
+
+apt install mono-devel
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
